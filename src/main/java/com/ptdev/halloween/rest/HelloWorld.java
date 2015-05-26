2c678f9453dab4b1d4edbf4c6308f2a3c9be3620
perry <perry.thomas@orasi.com>
2015-05-26 09:16:37 -0400

    Initial setup and example

---
+++ src/main/java/com/ptdev/halloween/rest/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.ptdev.halloween.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/test")
+public class HelloWorld {
+	@GET
+	@Path("/sayHello")
+	public Response sayHello( @QueryParam("from") String from, @QueryParam("to") String toPerson) {
+		String response = "<h1>" + from + " says hello to " + toPerson + "</h1>";
+		return Response.status(200).entity(response).build();
+	}
+}
