a8367ffcb115eb15aeeec46f64c9b4826a68f6f5
Perry Thomas <perry.thomas@inin.com>
2015-05-29 17:09:49 -0400

    -Started the command endpoint and configuration

---
+++ src/main/java/com/ptdev/rest/endpoints/Command.java
@@ -0,0 +1,21 @@
+package com.ptdev.rest.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.ptdev.picore.actions.*;
+
+@Path("/cmd")
+public class Command {
+	@GET
+	@Path("/{sequence}")
+	@Produces("application/json")
+	public Sequence sayHello( @PathParam("sequence") String seq) {
+		Sequence testSeq = new Sequence();
+		testSeq.addAction(new ActionRunner("test",new DigitalAction()));
+		
+		return testSeq;
+	}
+}
