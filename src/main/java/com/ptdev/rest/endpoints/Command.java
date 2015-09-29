f5a59094f58eddce85d0d61701720a61119ee6e8
Perry Thomas <perry.thomas@inin.com>
2015-09-28 21:52:34 -0400

    Finished command returns

--- src/main/java/com/ptdev/rest/endpoints/Command.java
+++ src/main/java/com/ptdev/rest/endpoints/Command.java
@@ -30,24 +30,23 @@ public class Command {
 	
 	@POST
 	@Path("/{sequence}")
-	public Response sayHello( @PathParam("sequence") String seq) {
+	public Response comResponse( @PathParam("sequence") String seq) {
 		//Run sequence based on name
 		try {
 			new ConfigReader(seq, mcpMap).getSequence().start();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return Response.status(404).entity("One of the files was not found: Stack - " + e.toString()).build();
 		} catch (YamlException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return Response.status(400).entity("Config file was not formatted right: Stack - " + e.toString()).build();
 		} catch (InvalidConfigSetupException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return Response.status(400).entity("Config file was not setup right: Stack - " + e.toString()).build();
 		} catch (ConfigDirectoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return Response.status(404).entity("Config file did not exist or was not set: Stack - " + e.toString()).build();
+		} catch (Exception e) {
+			return Response.status(500).entity("Oh shit! IDK LOLZ: Stack - " + e.toString()).build();
 		}
 		
+		//If no errors occurred then all should be well
 		return Response.status(200).entity(String.format("The '%s' command has been run successfully", seq)).build();
 	}
 }
