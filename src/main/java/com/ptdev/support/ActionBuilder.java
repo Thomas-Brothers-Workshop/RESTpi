e7a22a584fe02188cca46d15427127f1b72e4057
Perry Thomas <perry.thomas@inin.com>
2015-09-28 21:22:53 -0400

    added sound action to builder

--- src/main/java/com/ptdev/support/ActionBuilder.java
+++ src/main/java/com/ptdev/support/ActionBuilder.java
@@ -6,6 +6,8 @@ import com.ptdev.exceptions.InvalidConfigSetupException;
 import com.ptdev.picore.actions.BaseAction;
 import com.ptdev.picore.actions.DigitalAction;
 import com.ptdev.picore.actions.DigitalAction.IoState;
+import com.ptdev.picore.actions.SoundAction.SoundType;
+import com.ptdev.picore.actions.SoundAction;
 import com.ptdev.picore.io.IOsupport;
 import com.ptdev.picore.io.Mcp23017;
 
@@ -17,13 +19,12 @@ public class ActionBuilder {
 	public long time;
 	
 	public BaseAction build(Map<Integer, Mcp23017> mcpChips) throws InvalidConfigSetupException {
+		Mcp23017 mcpChip = mcpChips.get(IOsupport.getChipIndex(pin));
 		switch (this.type.toLowerCase()) {
 		case "io":
-			Mcp23017 mcpChip = mcpChips.get(IOsupport.getChipIndex(pin));
 			return new DigitalAction(mcpChip.outputPins.get(pin), IoState.getStateByString(set), time, delay);
 		case "sound":
-			//TODO implement this
-			return null;
+			return new SoundAction(mcpChip.outputPins.get(pin), SoundType.getTypeByString(set));
 		default:
 			throw new InvalidConfigSetupException(String.format("'%s' is an invalid type of action", this.type));
 		}
