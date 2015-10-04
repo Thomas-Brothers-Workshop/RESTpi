0c9fb1d9a66d115443688f2725af8d350b9bf3b9
Perry Thomas <perry.thomas@inin.com>
2015-10-04 11:17:51 -0400

    Adding singleton to contain IO

---
+++ src/main/java/com/ptdev/picore/io/IoContext.java
@@ -0,0 +1,57 @@
+package com.ptdev.picore.io;
+
+import java.util.Map;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.ptdev.picore.io.Mcp23017.ByteAddress;
+
+public class IoContext {
+	private static IoContext instance;
+	private GpioController gpio;
+	private Mcp23017 chipOne;
+	//private Mcp23017 chipTwo; 
+	private Map<Integer, Mcp23017> mcpMap;
+	private GpioPinDigitalOutput testPin;
+	
+	private IoContext() {
+		try {
+			//Chips
+			gpio = GpioFactory.getInstance();
+			System.out.println("GPIO Info: " + gpio.toString());
+			
+			
+			// set shutdown state for this pin
+			testPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+			testPin.setShutdownOptions(true, PinState.LOW);
+			
+			chipOne = new Mcp23017(gpio, ByteAddress.ONE, "one").setAllPinsOutput();
+			//chipTwo = new Mcp23017(GpioFactory.getInstance(), ByteAddress.TWO, "one").setAllPinsOutput();
+			
+			//Map
+			mcpMap.put(1, chipOne);
+//			mcpMap.put(2, chipTwo);
+		} catch (NullPointerException e) {
+			System.err.println("Looks like I couldn't find the IO expanders.");
+			e.printStackTrace();
+		}
+	}
+	
+	public static synchronized IoContext getInstance() {
+		if (instance == null)
+			instance = new IoContext();
+
+		return instance;
+	}
+	
+	public Map<Integer, Mcp23017> getMcpMap() {
+		return mcpMap;
+	}
+	
+	public GpioPinDigitalOutput getTestPin() {
+		return testPin;
+	}
+}
--- src/main/java/com/ptdev/rest/endpoints/Command.java
+++ src/main/java/com/ptdev/rest/endpoints/Command.java
@@ -1,8 +1,6 @@
 package com.ptdev.rest.endpoints;
 
 import java.io.FileNotFoundException;
-import java.util.Map;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,53 +8,22 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import com.esotericsoftware.yamlbeans.YamlException;
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 import com.ptdev.exceptions.ConfigDirectoryException;
 import com.ptdev.exceptions.InvalidConfigSetupException;
-import com.ptdev.picore.io.Mcp23017;
-import com.ptdev.picore.io.Mcp23017.ByteAddress;
+import com.ptdev.picore.io.IoContext;
+
 import com.ptdev.support.ConfigReader;
 
 @Path("/cmd")
 public class Command {
-	//Fix chip class to contain all chips
-	final GpioController gpio = GpioFactory.getInstance();
-	public Mcp23017 chipOne;
-	public Mcp23017 chipTwo; 
-	public Map<Integer, Mcp23017> mcpMap;
-	// provision gpio pin #01 as an output pin and turn on
-    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
 
-	public Command() {
-		try {
-			//Chips
-			System.out.println("GPIO Info: " + gpio.toString());
-			
-			// set shutdown state for this pin
-			pin.setShutdownOptions(true, PinState.LOW);
-			
-			chipOne = new Mcp23017(gpio, ByteAddress.ONE, "one").setAllPinsOutput();
-			//chipTwo = new Mcp23017(GpioFactory.getInstance(), ByteAddress.TWO, "one").setAllPinsOutput();
-			
-			//Map
-			mcpMap.put(1, chipOne);
-//			mcpMap.put(2, chipTwo);
-		} catch (NullPointerException e) {
-			System.err.println("Looks like I couldn't find the IO expanders.");
-			e.printStackTrace();
-		}
-	}
-	
 	@POST
 	@Path("/{sequence}")
 	public Response comResponse( @PathParam("sequence") String seq) {
 		//Run sequence based on name
 		try {
-			new ConfigReader(seq, mcpMap).getSequence().start();
+			IoContext.getInstance();
+			new ConfigReader(seq, IoContext.getInstance().getMcpMap()).getSequence().start();
 		} catch (FileNotFoundException e) {
 			return Response.status(404).entity("One of the files was not found: Stack - " + e.toString()).build();
 		} catch (YamlException e) {
@@ -81,14 +48,14 @@ public class Command {
 	@POST
 	@Path("/lightOn")
 	public Response turnLightOn() {
-		pin.setState(true);
+		IoContext.getInstance().getTestPin().setState(true);
 		return Response.status(200).entity("Light on").build();
 	}
 	
 	@POST
 	@Path("/lightOff")
 	public Response turnLightOff() {
-		pin.setState(false);
+		IoContext.getInstance().getTestPin().setState(false);
 		return Response.status(200).entity("Light on").build();
 	}
 }
