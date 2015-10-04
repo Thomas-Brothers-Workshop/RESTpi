9c60a623ae401e69888d499fb01797a77590cdfe
Perry Thomas <perry.thomas@inin.com>
2015-10-04 13:12:44 -0400

    Testing server

--- src/main/java/com/ptdev/support/ActionBuilder.java
+++ src/main/java/com/ptdev/support/ActionBuilder.java
@@ -6,8 +6,6 @@ import com.ptdev.picore.actions.DigitalAction;
 import com.ptdev.picore.actions.DigitalAction.IoState;
 import com.ptdev.picore.actions.SoundAction.SoundType;
 import com.ptdev.picore.actions.SoundAction;
-import com.ptdev.picore.io.IOsupport;
-import com.ptdev.picore.io.IoContext;
 import com.ptdev.picore.io.Mcp23017;
 
 public class ActionBuilder {
@@ -17,8 +15,7 @@ public class ActionBuilder {
 	public long delay;
 	public long time;
 	
-	public BaseAction build() throws InvalidConfigSetupException {
-		Mcp23017 mcpChip = IoContext.getInstance().getMcpMap().get(IOsupport.getChipIndex(pin));
+	public BaseAction build(Mcp23017 mcpChip) throws InvalidConfigSetupException {
 		switch (this.type.toLowerCase()) {
 		case "io":
 			return new DigitalAction(mcpChip.outputPins.get(pin), IoState.getStateByString(set), time, delay);
